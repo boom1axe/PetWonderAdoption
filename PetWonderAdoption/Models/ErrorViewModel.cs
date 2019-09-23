9dabfdf974fbae7989fbcefe362d7e5187a5eb01
Dennis Kafton <boom1axe@att.net>
2019-09-23 15:12:46 -0700

    Add project files.

---
+++ PetWonderAdoption/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PetWonderAdoption.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
