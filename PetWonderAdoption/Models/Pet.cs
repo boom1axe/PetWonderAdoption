925ce7fd2ebf80b1fbbc47c158fcd00cc781dce6
Dennis Kafton <boom1axe@att.net>
2019-12-02 15:45:36 -0800

    Created starting class for pets

---
+++ PetWonderAdoption/Models/Pet.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PetWonderAdoption.Models
+{
+    public class Pet
+    {
+        /// <summary>
+        /// Unique ID number for the game
+        /// </summary>
+        [Key]
+        public int Id { get; set; }
+
+        /// <summary>
+        /// The title the pet will be called as
+        /// </summary>
+        [Required]
+        [StringLength(90)]
+        public string Title { get; set; }
+
+        /// <summary>
+        /// The type of catagory the pet counts as
+        /// </summary>
+        public string Breed { get; set; }
+
+        /// <summary>
+        /// market price for pet
+        /// </summary>
+        public string Description { get; set; }
+        [Range(0.01, 999.99)]
+        [DataType(DataType.Currency)]
+        public double Price { get; set; }
+    }
+}
